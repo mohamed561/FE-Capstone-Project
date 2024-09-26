0f53f8f22a3af4fea1ed673cd15776767324bb3d
mohamed561 <mrajawi561@gmail.com>
2024-09-26 23:32:28 +0100

    editing the header

--- movie-database/src/components/Header.js
+++ movie-database/src/components/Header.js
@@ -1,19 +1,28 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import SearchBar from './SearchBar';
 
 function Header() {
   return (
     <header className="bg-blue-600 text-white p-4">
       <div className="container mx-auto flex justify-between items-center">
-        <Link to="/" className="text-2xl font-bold">MovieDB</Link>
-        <nav>
-          <ul className="flex space-x-4">
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/favorites">Favorites</Link></li>
+        {/* Logo */}
+        <Link to="/">
+          <img src="./sources/logo.png" alt="Logo" className="h-10" />
+        </Link>
+
+        {/* Centered Navigation */}
+        <nav className="flex-grow">
+          <ul className="flex justify-center space-x-8">
+            <li><Link to="/" className="hover:text-gray-300">Home</Link></li>
+            <li><Link to="/favorites" className="hover:text-gray-300">Favorites</Link></li>
           </ul>
         </nav>
-        <SearchBar />
+
+        {/* Login and Signup Buttons */}
+        <div className="flex space-x-4">
+          <Link to="/login" className="bg-white text-blue-600 px-4 py-2 rounded hover:bg-gray-200">Login</Link>
+          <Link to="/signup" className="bg-white text-blue-600 px-4 py-2 rounded hover:bg-gray-200">Signup</Link>
+        </div>
       </div>
     </header>
   );
