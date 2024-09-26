45fc86ec87e167545f0e6a70d0fbfca5b5aef2c4
mohamed561 <mrajawi561@gmail.com>
2024-09-26 21:37:41 +0100

    solving a problem with vercel

--- movie-database/src/services/api.js
+++ movie-database/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_KEY = 'e9b834';
-const BASE_URL = 'http://www.omdbapi.com/';
+const BASE_URL = 'https://www.omdbapi.com/';
 
 export const searchMovies = async (query) => {
   const response = await axios.get(`${BASE_URL}?apikey=${API_KEY}&s=${query}`);
