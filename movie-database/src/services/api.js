31c72b5102439333fe011764a9587e864ab09aed
mohamed561 <mrajawi561@gmail.com>
2024-09-27 00:49:58 +0100

    restoring old api

--- movie-database/src/services/api.js
+++ movie-database/src/services/api.js
@@ -1,35 +1,14 @@
-// const API_KEY = 'e9b834';
-// const BASE_URL = 'https://www.omdbapi.com/';
-
 import axios from 'axios';
 
 const API_KEY = 'e9b834';
 const BASE_URL = 'https://www.omdbapi.com/';
 
 export const searchMovies = async (query) => {
-  const response = await axios.get(`${BASE_URL}/search/movie`, {
-    params: {
-      api_key: API_KEY,
-      query: query,
-    }
-  });
-  return response.data.results;
-};
-
-export const getMovieDetails = async (id) => {
-  const response = await axios.get(`${BASE_URL}/movie/${id}`, {
-    params: {
-      api_key: API_KEY,
-    }
-  });
+  const response = await axios.get(`${BASE_URL}?apikey=${API_KEY}&s=${query}`);
   return response.data;
 };
 
-export const getPopularMovies = async () => {
-  const response = await axios.get(`${BASE_URL}/movie/popular`, {
-    params: {
-      api_key: API_KEY,
-    }
-  });
-  return response.data.results;
+export const getMovieDetails = async (id) => {
+  const response = await axios.get(`${BASE_URL}?apikey=${API_KEY}&i=${id}`);
+  return response.data;
 };
