1b9c1aee45d87a982b012c5cfa2f7f34e817aaf0
mohamed561 <mrajawi561@gmail.com>
2024-09-27 23:16:27 +0100

    working on firebase

---
+++ movie-database/src/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBCz6QNBczhCeNYFPkD6nb396D6cJGD_a4",
+  authDomain: "mdb-app-55ae9.firebaseapp.com",
+  projectId: "mdb-app-55ae9",
+  storageBucket: "mdb-app-55ae9.appspot.com",
+  messagingSenderId: "1092521009012",
+  appId: "1:1092521009012:web:e3188e9fb3d2d87c29d621",
+  measurementId: "G-SHZ5LWXFFY"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
