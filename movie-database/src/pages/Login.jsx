16d131d3b759eaa80ed911e4c4b655d170f187f1
mohamed561 <mrajawi561@gmail.com>
2024-09-28 00:05:30 +0100

    restoring order

--- movie-database/src/pages/Login.jsx
+++ movie-database/src/pages/Login.jsx
@@ -22,7 +22,7 @@ function Login() {
   };
 
   return (
-    <div className="flex justify-center items-center h-screen bg-gray-100">
+    <div className="flex justify-center items-center h-screen" style={{ backgroundColor: '#282c34' }}>
       <div className="bg-white p-8 rounded-lg shadow-lg w-full max-w-md">
         <h2 className="text-3xl font-bold text-center mb-8 text-blue-600">Login to Your Account</h2>
         <form onSubmit={handleLogin}>
--- movie-database/src/pages/Signup.jsx
+++ movie-database/src/pages/Signup.jsx
@@ -27,7 +27,7 @@ function Signup() {
   };
 
   return (
-    <div className="flex justify-center items-center h-screen bg-gray-100">
+    <div className="flex justify-center items-center h-screen" style={{ backgroundColor: '#282c34' }}>
       <div className="bg-white p-8 rounded-lg shadow-lg w-full max-w-md">
         <h2 className="text-3xl font-bold text-center mb-8 text-blue-600">Create an Account</h2>
         <form onSubmit={handleSignup}>
