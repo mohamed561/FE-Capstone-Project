72919fbc8fdb2731af8fd64b0453aa90eda0b966
mohamed561 <mrajawi561@gmail.com>
2024-09-28 19:13:31 +0100

    edit's on favorites

--- movie-database/src/pages/MovieDetailsPage.js
+++ movie-database/src/pages/MovieDetailsPage.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useParams, Link } from 'react-router-dom';
 import { getMovieDetails } from '../services/api';
+import { getMovieDetails } from '../services/tmdbApi';
 
 function MovieDetailsPage() {
   const [movie, setMovie] = useState(null);
